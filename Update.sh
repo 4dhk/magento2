570a2f53e56202a4ccef94c2e995e89e825bbfc0
Patrick Ng <patrickng@4d.com.hk>
2017-04-01 14:32:45 +0800

    add update script

---
+++ Update.sh
@@ -0,0 +1,29 @@
+#sh
+
+
+
+### This script is assume that the repo is under [project folder]/submodules/smartwaveporto.
+### Folder name is not a problem
+case "$(uname)" in
+	Darwin)
+		DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	;;
+	Linux)
+		DIR="$( cd "$( dirname $(readlink -f $0) )" && pwd )"
+	;;
+	*)
+		DIR="$( cd "$( dirname $(readlink -f $0) )" && pwd )"
+	;;
+esac
+TargetVendorMagentoDir=$DIR/../../vendor/magento
+
+echo "Workding Directory:"$DIR
+echo "Vender Magento Directory:"$TargetVendorMagentoDir
+
+chown -R ubuntu ./*
+
+cp -Rp $DIR/app/code/Magento/Catalog/* $TargetVendorMagentoDir/module-catalog
+
+chgrp -R www-data ./*
+
+
